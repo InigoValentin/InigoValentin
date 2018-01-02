baebec86a0d46635466e5fa82912b22778df5101
Iñigo Valentin <i@inigovalentin.com>
2018-01-02 16:02:47 +0100

    Error pages reworked.

--- www/error.php
+++ www/error.php
@@ -9,7 +9,11 @@
     $lang = select_language($con);
     $lserver = $server . "/" . $lang;
     $cur_section = "error";
-    $cur_entry = "403";
+    $cur_entry = http_response_code();
+    $error = $cur_entry;
+    if (!in-array($error, array("400", "401", "403", "404", "500")){
+        $error = "XXX";
+    }
 ?>
 <!DOCTYPE html>
 <html>
@@ -18,8 +22,6 @@
         <meta name='viewport' content='width=device-width, initial-scale=1, maximum-scale=1, minimum-scale=1'>
         <title><?=text($con, "ERROR_TITLE", $lang);?> - <?=text($con, "USER_NAME", $lang);?></title>
         <link rel='shortcut icon' href='<?=$lserver?>/img/logo/favicon.ico'>
-        <!-- CSS files -->
-        <link rel='stylesheet' type='text/css' href='/css/ui.css'/>
                 <!-- CSS files -->
         <style>
 <?php
@@ -40,32 +42,42 @@
 <?php
         include $doc_root . "header.php";
 ?>
-        <div class='section section_no_title' >
-            <table id='error-code' class='entry'>
+        <div class='section' >
+            <h3 class='section_title'><?=text($con, "ERROR_ERROR", $lang)?></h3>
+            <table id='error_header'>
                 <tr>
-                    <td id='error-number'>
-                        403
+                    <td>
+                        <table id='error_code' class='entry'>
+                            <tr>
+                                <td id='error_number'>
+                                    404
+                                </td>
+                                <td id='error_tag'>
+                                    <?=text($con, "ERROR_ERROR", $lang);?>
+                                    <br/>
+                                    <?=text($con, "ERROR_CODE", $lang);?>
+                                </td>
+                            <tr>
+                        </table>
                     </td>
-                    <td id='error-tag'>
-                        <?=text($con, "ERROR_DISPLAY", $lang);?>
+                    <td>
+                        <h2><?=text($con, "ERROR_" . $error . "_TITLE", $lang);?></h2>
                     </td>
-                <tr>
+                </tr>
             </table>
-            <br/><br/><br/><br/>
-            <div id='error-message' class='entry'>
-                <h2><?=text($con, "ERROR_403", $lang);?></h2>
-                <?=text($con, "ERROR_403_DESC", $lang);?>
+            <div id='error_message' class='entry'>
+                <?=text($con, "ERROR_" . $error . "_DESCRIPTION", $lang);?>
                 <br/><br/>
-                <?=text($con, "ERROR_TRY", $lang);?>
+                <?=text($con, "ERROR_" . $error . "_SOLUTION", $lang);?>
                 <ul>
                     <li>
-                        <a href='javascript: location.reload();'><?=text($con, "ERROR_SOLUTION_0", $lang);?></a>
+                        <a href='javascript: location.reload();'><?=text($con, "ERROR_" . $error . "_SOLUTION_0", $lang);?></a>
                     </li>
                     <li>
-                        <a href='javascript: history.go(-1);'><?=text($con, "ERROR_SOLUTION_1", $lang);?></a>
+                        <a href='javascript: history.go(-1);'><?=text($con, "ERROR_" . $error . "_SOLUTION_1", $lang);?></a>
                     </li>
                     <li>
-                        <a href='<?=$lserver?>/'><?=text($con, "ERROR_SOLUTION_2", $lang);?></a>
+                        <a href='<?=$lserver?>/'><?=text($con, "ERROR_" . $error . "_SOLUTION_2", $lang);?></a>
                     </li>
                 </ul>
             </div> <!-- #error_message -->
@@ -73,6 +85,7 @@
 <?php
         include $doc_root . "footer.php";
         stats($con, $cur_section, $cur_entry);
+        http_response_code($cur_entry);
 ?>
     </body>
 </html>
